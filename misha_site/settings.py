310a4e50127e620a52699ec59db1e8957dca448f
misha-korn <mishaizh.korn@gmail.com>
2024-10-28 17:42:14 +0400

    Debug False

--- misha_site/settings.py
+++ misha_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-20%tt*-u#orz=zu1nifftvboms&*hl#-1fi0zxg@_b64w+h1a_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["185.154.195.78", "127.0.0.1"]
 
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
